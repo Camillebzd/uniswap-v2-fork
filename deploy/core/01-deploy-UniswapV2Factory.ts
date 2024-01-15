3e3ca75ab58273d7e144ae2d29fff52a69012e24
CamilleBzd <bcamille99@gmail.com>
2024-01-15 16:34:23 +0100

    [ADD] core part and test on local context

---
+++ deploy/core/01-deploy-UniswapV2Factory.ts
@@ -0,0 +1,31 @@
+import { DeployFunction } from "hardhat-deploy/dist/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { blockConfirmation, developmentChains } from "../../helper-hardhat-config";
+import { verify } from "../../scripts/utils/verify";
+
+const deployUniswapV2Factory: DeployFunction = async function(
+  hre: HardhatRuntimeEnvironment
+) {
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  log("----------------------------------------------------");
+  log("Deploying UniswapV2Factory and waiting for confirmations...");
+  const uniswapV2Factory = await deploy("UniswapV2Factory", {
+    from: deployer,
+    args: [deployer],
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: blockConfirmation[network.name] || 1,
+  });
+
+  // verify if not on a local chain
+  if (!developmentChains.includes(network.name)) {
+    console.log("Wait before verifying");
+    await verify(uniswapV2Factory.address, [deployer]);
+  }
+};
+
+export default deployUniswapV2Factory;
+deployUniswapV2Factory.tags = ["all", "core", "Factory", "UniswapV2Factory"];
