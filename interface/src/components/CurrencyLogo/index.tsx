f0416f898f038a08898d1b6079a7c3797b3e2424
CamilleBzd <bcamille99@gmail.com>
2024-02-15 09:35:37 +0100

    [FIX] currency logo on pool page

--- interface/src/components/CurrencyLogo/index.tsx
+++ interface/src/components/CurrencyLogo/index.tsx
@@ -7,8 +7,16 @@ import useHttpLocations from '../../hooks/useHttpLocations'
 import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
 
+const tokenURLs: {[key: string]: string} = {
+  "0x0856b832aDeD7E34CF685160D53c12Bd96513448": "https://s3.amazonaws.com/roll-token.tryroll.com/USDC/46907d92-3d53-4302-8342-1b260faf376d", // USDC
+  "0xB018f181E61eA2c5c223d15CF78D9ed77Bcc57b0": "https://s3.amazonaws.com/roll-token.tryroll.com/USDT/2a996945-ceb5-4d18-8455-4af5e4d1d9d5", // USDT
+  "0x217271A8674a5362F5221dD040f7c44C28f2c046": "https://s3.amazonaws.com/roll-token.tryroll.com/DAI/8405fc15-2269-4228-b69e-79a6717eb15b", // DAI
+  "0xF6a785b301CE4F1610CE30E435AA41Ed08d77220": "https://s3.amazonaws.com/roll-token.tryroll.com/WETH/8b5214dc-7b7e-4e04-89de-f0f4ae05a3b6" // WETH
+}
+
 const getTokenLogoURL = (address: string) =>
-  `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+  // `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+  tokenURLs[address]
 
 const StyledEthereumLogo = styled.img<{ size: string }>`
   width: ${({ size }) => size};
