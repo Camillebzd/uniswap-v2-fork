3ef220a45385f521b89352e455c3d77b20df696d
CamilleBzd <bcamille99@gmail.com>
2024-03-03 10:55:56 +0100

    [FIX] new token list and add public url for token list on pinata

--- interface/src/constants/lists.ts
+++ interface/src/constants/lists.ts
@@ -1,6 +1,6 @@
 // the Uniswap Default token list lives here
 // export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
-export const DEFAULT_TOKEN_LIST_URL = 'https://salmon-fancy-leopard-207.mypinata.cloud/ipfs/QmVJjtkm72yTsMuk4bdUKgAJ6pNGYHYFydV6Kka7VVVTft'
+export const DEFAULT_TOKEN_LIST_URL = 'https://gateway.pinata.cloud/ipfs/QmRPFf3fkqaknRZWyLF4YCNF61FK9pYuHRWxg648Z9cwx4'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
