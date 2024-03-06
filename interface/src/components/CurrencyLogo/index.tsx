5463074a54aef1c1c6f27e849b3099b25cfdd5c5
CamilleBzd <bcamille99@gmail.com>
2024-03-06 18:35:46 +0100

    [FIX] token logo url in the currencyLogo component

--- interface/src/components/CurrencyLogo/index.tsx
+++ interface/src/components/CurrencyLogo/index.tsx
@@ -9,9 +9,9 @@ import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
 
 const tokenURLs: {[key: string]: string} = {
-  "0x0856b832aDeD7E34CF685160D53c12Bd96513448": "https://s3.amazonaws.com/roll-token.tryroll.com/USDC/46907d92-3d53-4302-8342-1b260faf376d", // USDC
-  "0xB018f181E61eA2c5c223d15CF78D9ed77Bcc57b0": "https://s3.amazonaws.com/roll-token.tryroll.com/USDT/2a996945-ceb5-4d18-8455-4af5e4d1d9d5", // USDT
-  "0x217271A8674a5362F5221dD040f7c44C28f2c046": "https://s3.amazonaws.com/roll-token.tryroll.com/DAI/8405fc15-2269-4228-b69e-79a6717eb15b", // DAI
+  "0x07648428EF73dde0ba38D3775C6aaDc48fD20d9D": "https://s3.amazonaws.com/roll-token.tryroll.com/USDC/46907d92-3d53-4302-8342-1b260faf376d", // USDC
+  "0x42b2ae83596fA9f4D8D9958f571a10934ED5B2cc": "https://s3.amazonaws.com/roll-token.tryroll.com/USDT/2a996945-ceb5-4d18-8455-4af5e4d1d9d5", // USDT
+  "0xb8f26e0c5cc99Cdd8A42Ae8A8BaEbB20ecBD1C99": "https://s3.amazonaws.com/roll-token.tryroll.com/DAI/8405fc15-2269-4228-b69e-79a6717eb15b", // DAI
   "0xF6a785b301CE4F1610CE30E435AA41Ed08d77220": "https://s3.amazonaws.com/roll-token.tryroll.com/WETH/8b5214dc-7b7e-4e04-89de-f0f4ae05a3b6" // WETH
 }
 
