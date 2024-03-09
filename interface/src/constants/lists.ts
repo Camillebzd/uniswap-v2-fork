dd7bd264d56cd0d08d87b57621cf056e5d997a77
CamilleBzd <bcamille99@gmail.com>
2024-03-09 13:58:57 +0100

    [FIX] change the default token list by using github url instead of pinata url

--- interface/src/constants/lists.ts
+++ interface/src/constants/lists.ts
@@ -1,6 +1,6 @@
 // the Uniswap Default token list lives here
 // export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
-export const DEFAULT_TOKEN_LIST_URL = 'https://gateway.pinata.cloud/ipfs/QmRPFf3fkqaknRZWyLF4YCNF61FK9pYuHRWxg648Z9cwx4'
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/Camillebzd/uniswap-v2-fork/main/interface/tokens_list.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
